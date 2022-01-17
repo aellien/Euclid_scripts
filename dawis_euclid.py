212b86c3ddf69f49c90a6d7b3f840526400ef727
aellien <a.r.j.ellien@uva.nl>
2022-01-17 14:38:27 +0100

    fix

--- dawis_euclid.py
+++ dawis_euclid.py
@@ -6,10 +6,10 @@ indir = '/home/ellien/Euclid_ICL/simulations/out1'
 infile = 'ICL_V_bright.fits'
 outdir = '/home/ellien/Euclid_ICL/wavelets/out2'
 n_cpus = 4 # Number of CPUs
-tau = 0.5   # Relative Threshold
-gamma = 0.2   # Attenuation (CLEAN) factor
+tau = 0.1   # Relative Threshold
+gamma = 0.5   # Attenuation (CLEAN) factor
 ceps = 1E-5    # Convergence value for epsilon
-n_levels = 11    # Number of wavelet scales
+n_levels = 12    # Number of wavelet scales
 min_span = 2    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
 max_span = 3    # Maximum number of wavelet scales spanned by an interscale tree
 lvl_sep_big = 7     # Scale at wich mix_span & max_span are set to 1
@@ -19,8 +19,9 @@ max_iter = 1500      # Maximum number of iterations
 data_dump = True    # Write data at each iteration /!\ demands lot of space on hardware /!\
 gif = True      # Make gifs of the run (need data_dump = True)
 starting_level = 2 # Starting wavelet scale (this is the third scale - Python convention 0 1 2)
+conditions = 'prolongation'
 
 dawis.synthesis_by_analysis( indir = indir, infile = infile, outdir = outdir, n_cpus = n_cpus, n_levels = n_levels, \
-                                    tau = tau, gamma = gamma, ceps = ceps, min_span = min_span, \
+                                    tau = tau, gamma = gamma, ceps = ceps, conditions = conditions, min_span = min_span, \
                                     max_span = max_span, lvl_sep_big = lvl_sep_big, max_iter = max_iter, \
                                     data_dump = data_dump, gif = gif )
