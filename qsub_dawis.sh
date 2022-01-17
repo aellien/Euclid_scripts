7f64fe485ed6d5d31763842678b57966df1526b0
aellien <a.r.j.ellien@uva.nl>
2022-01-17 14:36:25 +0100

    add qsub script

---
+++ qsub_dawis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -o /home/ellien/Euclid_ICL/logs/icl_euclid.out
+#PBS -j oe
+#PBS -N icl_euclid
+#PBS -l nodes=1:ppn=4,walltime=96:00:00
+#PSB -S /bin/bash
+
+module load intelpython/3-2020.4
+python /home/ellien/Euclid_ICL/scripts/dawis_euclid.py
+
+
+exit 0
