81216a2ddeee9dacf126fc7d56ac78d2e1d582c0
aellien <a.r.j.ellien@uva.nl>
2022-01-21 15:10:21 +0100

    change par

--- dawis_euclid.py
+++ dawis_euclid.py
@@ -6,9 +6,9 @@ indir = '/home/ellien/Euclid_ICL/simulations/out1'
 infile = 'ICL_V_bright.fits'
 outdir = '/n03data/ellien/Euclid_ICL/wavelets/out3'
 n_cpus = 4 # Number of CPUs
-tau = 0.5   # Relative Threshold
-gamma = 0.8   # Attenuation (CLEAN) factor
-ceps = 1E-6    # Convergence value for epsilon
+tau = 0.8   # Relative Threshold
+gamma = 0.2   # Attenuation (CLEAN) factor
+ceps = 5E-7    # Convergence value for epsilon
 n_levels = 11    # Number of wavelet scales
 min_span = 1    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
 max_span = 3    # Maximum number of wavelet scales spanned by an interscale tree
