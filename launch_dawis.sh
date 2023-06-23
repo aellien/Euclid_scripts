8918a83b8a93fb1803ea1a4b2b764c06a5b4d4f5
ellien <ellien@iap.fr>
2023-06-23 19:41:11 +0200

    launch dawis

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -1,33 +1,8 @@
 #!/bin/bash
-#
-# Challenge1_Euclid0.iptd.rebin.fits  Challenge2_Euclid1.iptd.rebin.fits  Challenge3_Euclid1.iptd.rebin.fits
-# Challenge1_Euclid1.iptd.rebin.fits  Challenge2_Euclid2.iptd.rebin.fits  Challenge3_Euclid2.iptd.rebin.fits
-# Challenge1_Euclid2.iptd.rebin.fits  Challenge2_Euclid3.iptd.rebin.fits  Challenge3_Euclid3.iptd.rebin.fits
-# Challenge1_Euclid3.iptd.rebin.fits  Challenge2_Euclid4.iptd.rebin.fits  Challenge3_Euclid4.iptd.rebin.fits
-# Challenge1_Euclid4.iptd.rebin.fits  Challenge2_Euclid5.iptd.rebin.fits  Challenge3_Euclid5.iptd.rebin.fits
-# Challenge2_Euclid0.iptd.rebin.fits  Challenge3_Euclid0.iptd.rebin.fits
-#  Challenge2_Euclid1.iptd.rebin.fits  Challenge3_Euclid1.iptd.rebin.fits Challenge1_Euclid1.iptd.rebin.fits  Challenge2_Euclid2.iptd.rebin.fits  Challenge3_Euclid2.iptd.rebin.fits Challenge1_Euclid2.iptd.rebin.fits  Challenge2_Euclid3.iptd.rebin.fits  Challenge3_Euclid3.iptd.rebin.fits Challenge1_Euclid3.iptd.rebin.fits  Challenge2_Euclid4.iptd.rebin.fits  Challenge3_Euclid4.iptd.rebin.fits Challenge1_Euclid4.iptd.rebin.fits  Challenge2_Euclid5.iptd.rebin.fits  Challenge3_Euclid5.iptd.rebin.fits Challenge2_Euclid0.iptd.rebin.fits  Challenge3_Euclid0.iptd.rebin.fits
 
-
-for file in Challenge2_Euclid2.iptd.rebin.fits \
-            Challenge2_Euclid1.iptd.rebin.fits \
-            Challenge2_Euclid3.iptd.rebin.fits \
-            Challenge2_Euclid4.iptd.rebin.fits \
-            Challenge2_Euclid5.iptd.rebin.fits \
-            Challenge3_Euclid1.iptd.rebin.fits \
-            Challenge1_Euclid1.iptd.rebin.fits \
-            Challenge3_Euclid2.iptd.rebin.fits \
-            Challenge1_Euclid2.iptd.rebin.fits \
-            Challenge3_Euclid3.iptd.rebin.fits \
-            Challenge1_Euclid3.iptd.rebin.fits \
-            Challenge3_Euclid4.iptd.rebin.fits \
-            Challenge1_Euclid4.iptd.rebin.fits \
-            Challenge3_Euclid5.iptd.rebin.fits \
-            Challenge2_Euclid0.iptd.rebin.fits \
-            Challenge3_Euclid0.iptd.rebin.fits
+for file in /n03data/ellien/Euclid_ICL/simulations/*crop*
 
 do
       echo "Launch Dawis on file ${file}"
-      qsub qsub_dawis.sh -v ncl=${file:0:29},nf=${file}
-      sleep 2
+      echo "qsub qsub_dawis.sh -v ncl=${file:3:6},nf=${file}"
 done
