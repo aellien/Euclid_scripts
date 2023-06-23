346a0d8ff012049396f2b9187321d1da62caef4e
ellien <ellien@iap.fr>
2023-06-23 19:46:56 +0200

    230620231947

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -5,6 +5,5 @@ for file in /n03data/ellien/Euclid_ICL/simulations/out4/*crop*
 do
       echo "Launch Dawis on file ${file}"
       n=$(basename "$file")
-      echo $n
-      echo "qsub qsub_dawis.sh -v ncl=${n:3:3},nf=${file}"
+      qsub qsub_dawis.sh -v ncl=${n:3:3},nf=${file}
 done
