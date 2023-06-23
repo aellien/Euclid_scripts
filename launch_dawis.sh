18d650567db3d104beebd6ccfc43c8cc94d77bcc
ellien <ellien@iap.fr>
2023-06-23 19:44:32 +0200

    test

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -4,5 +4,6 @@ for file in /n03data/ellien/Euclid_ICL/simulations/*crop*
 
 do
       echo "Launch Dawis on file ${file}"
-      echo "qsub qsub_dawis.sh -v ncl=${file:3:6},nf=${file}"
+      n=$(basename "$file")
+      echo "qsub qsub_dawis.sh -v ncl=${n:3:6},nf=${file}"
 done
--- qsub_dawis.sh
+++ qsub_dawis.sh
@@ -2,12 +2,12 @@
 #PBS -o /home/ellien/Euclid_ICL/logs/${ncl}.out
 #PBS -j oe
 #PBS -N icl_euclid
-#PBS -l nodes=1:ppn=2,walltime=47:00:00
+#PBS -l nodes=1:ppn=4,walltime=47:00:00
 #PSB -S /bin/bash
 
 module load intelpython/3-2020.4
 echo ${ncl}
 echo ${nf}
-python /home/ellien/Euclid_ICL/Euclid_scripts/dawis_euclid.py ${nf}
+python /home/ellien/Euclid_ICL/Euclid_scripts/dawis_${ncl}.py ${nf}
 
 exit 0
