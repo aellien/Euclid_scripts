d81bd9ca68316bf2992508931571c68fbe82a29b
ellien <ellien@iap.fr>
2023-09-25 16:51:43 +0200

    out6 run8

--- dawis_NIR.py
+++ dawis_NIR.py
@@ -5,15 +5,16 @@ import shutil
 
 indir = '/n03data/ellien/Euclid_ICL/simulations/out6'
 infile = sys.argv[1]
-outdir = '/n03data/ellien/Euclid_ICL/wavelets/out6/run7'
+outdir = '/n03data/ellien/Euclid_ICL/wavelets/out6/run8'
 n_cpus = 4 # Number of CPUs
+n_sigmas = 3
 tau = 0.1   # Relative Threshold
 gamma = 0.2   # Attenuation (CLEAN) factor
-ceps = 1E-4    # Convergence value for epsilon
-n_levels = 12    # Number of wavelet scales
+ceps = 1E-5    # Convergence value for epsilon
+n_levels = 10    # Number of wavelet scales
 min_span = 1    # Minimum of wavelet scales spanned by an interscale tree (must be >= 1)
 max_span = 2    # Maximum number of wavelet scales spanned by an interscale tree
-lvl_sep_big = 10     # Scale at wich mix_span, max_span & gamma are set to 1, and monomodality is enforced
+lvl_sep_big = 5     # Scale at wich mix_span, max_span & gamma are set to 1, and monomodality is enforced
 extent_sep = 0.15   # Ratio n_pix/vignet under which the Haar wavelet is used for restoration
 ecc_sep = 0.9      # Eccentricity threshold over which the Haar wavelet is used for restoration
 lvl_sep_lin = 3     # Wavelet scale under which the Haar wavelet can be used for restoration
@@ -24,13 +25,13 @@ starting_level = 2 # Starting wavelet scale (this is the third scale - Python co
 conditions = 'prolongation'
 monomodality = True
 resume = True
-rm_gamma_for_big = True
+rm_gamma_for_big = False
 
 if os.path.isdir( outdir ) == False:
     os.makedirs( outdir, exist_ok = True )
 shutil.copyfile( os.path.abspath(__file__), os.path.join( outdir, infile[:-4] + 'input.dawis.py' ) )
 
-dawis.synthesis_by_analysis( indir = indir, infile = infile, outdir = outdir, n_cpus = n_cpus, n_levels = n_levels, \
+dawis.synthesis_by_analysis( indir = indir, infile = infile, outdir = outdir, n_cpus = n_cpus, n_levels = n_levels, n_sigmas = n_sigmas, \
                                     tau = tau, gamma = gamma, ceps = ceps, conditions = conditions, min_span = min_span, \
                                     max_span = max_span, lvl_sep_big = lvl_sep_big, monomodality = monomodality, \
                                     max_iter = max_iter, extent_sep = extent_sep, ecc_sep = ecc_sep, rm_gamma_for_big = rm_gamma_for_big, \
