c55a2815039153ffb2c8b92218a599d24100c219
aellien <ellien@iap.fr>
2024-04-02 21:37:34 +0200

    fix bug

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -7,7 +7,8 @@ do
       echo "Launch Dawis on file ${file}"
       n=$(basename "$file")
       ncl=${cluster_num}_${n:0:-5}
+      indir=/n03data/ellien/Euclid_ICL/simulations/out2/${cluster_num}/
       outdir=/n03data/ellien/Euclid_ICL/wavelets/out2/${cluster_num}
-      qsub qsub_dawis.sh -v ncl=${ncl},nf=${n},indir=${file},outdir=${outdir}
+      qsub qsub_dawis.sh -v ncl=${ncl},nf=${n},indir=${indir},outdir=${outdir}
       done
 done
