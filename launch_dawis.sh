d6c653740c34a7b10a73da6adc5de9dc1006723d
aellien <ellien@iap.fr>
2024-04-03 14:41:05 +0200

    test bug mpi

--- launch_dawis.sh
+++ launch_dawis.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-
+njobs=0
 for cluster_num in 365000132000018 360009933000018 373000139000019
 do
     for file in /n03data/ellien/Euclid_ICL/simulations/out2/${cluster_num}/*
     do
+      njobs=$((${njobs}+1))
       echo "Launch Dawis on file ${file}"
       n=$(basename "$file")
       ncl=${cluster_num}_${n:0:-5}
       indir=/n03data/ellien/Euclid_ICL/simulations/out2/${cluster_num}/
       outdir=/n03data/ellien/Euclid_ICL/wavelets/out2/${cluster_num}
       qsub qsub_dawis.sh -v ncl=${ncl},nf=${n},indir=${indir},outdir=${outdir}
-      done
+      if njobs -gt 50
+      then
+          break
+      fi
+   done
+   if njobs -gt 24
+   then
+       break
+   fi
 done
