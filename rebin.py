f4e9083feb965d6aca85f16944739613a921d2bc
aellien <ellien@iap.fr>
2024-04-17 13:55:09 +0200

    wavsizesepbcg synth sim

--- rebin.py
+++ rebin.py
@@ -1,63 +1,41 @@
-def rebin_fits(FILENAME,XBIN=2,YBIN=2):
+import os
+import sys
+import glob
+import numpy as np
 
-    #---------------------------------------------------------------------------
-    # MODULES
-    from astropy.io import fits
-    import pylab as plt
-    import numpy as np
+def rebin(im, xbin = 2, ybin = 2, btype = 'sum'):
 
-    #---------------------------------------------------------------------------
-    # LECTURE FILE
-    fitsFile = fits.open(FILENAME)
-    im = np.array(fitsFile[0].data)
-    #im = im[:7040,:7040]
-    #print(np.shape(im))
+    print('Input shape: %d x %d' %( im.shape[0], im.shape[1] ))
+    print('XBIN = %d, YBIN = %d' %( xbin, ybin ))
 
-    #---------------------------------------------------------------------------
-    # BINNAGE
-    xedge = np.shape(im)[0]%XBIN
-    yedge = np.shape(im)[1]%YBIN
+    xedge = np.shape(im)[0]%xbin
+    yedge = np.shape(im)[1]%ybin
     im = im[xedge:,yedge:]
-    binim = np.reshape(im,(int(np.shape(im)[0]/XBIN),XBIN,int(np.shape(im)[1]/YBIN),YBIN))
-    binim = np.mean(binim,axis=3)
-    binim = np.mean(binim,axis=1)
+    binim = np.reshape(im,(int(np.shape(im)[0]/xbin),xbin,int(np.shape(im)[1]/ybin),ybin))
 
-    #---------------------------------------------------------------------------
-    # NEW FILE
-    oldheader = fitsFile[0].header
-    newheader = oldheader
-    try:
-        newheader['CRPIX1'] = oldheader['CRPIX1']/float(XBIN)
-        newheader['CD1_1'] = oldheader['CD1_1']*float(XBIN)
-        newheader['CD1_2'] = oldheader['CD1_2']*float(XBIN)
-        newheader['CRPIX2'] = oldheader['CRPIX2']/float(YBIN)
-        newheader['CD2_2'] = oldheader['CD2_2']*float(YBIN)
-        newheader['CD2_1'] = oldheader['CD2_1']*float(YBIN)
-    except:
-        pass
+    if type == 'MEAN':
+        binim = np.mean(binim,axis=3)
+        binim = np.mean(binim,axis=1)
+    elif type == 'SUM':
+        binim = np.sum(binim,axis=3)
+        binim = np.sum(binim,axis=1)
+    print('New shape: %d x %d' %( binim.shape[0], binim.shape[1]  ))
 
-    hdu = fits.PrimaryHDU(binim,header=newheader)
-    hdu.writeto(FILENAME[:-4]+'rebin.fits',overwrite=True)
+    return binim
 
 if __name__ == '__main__':
 
-    import glob as glob
-    import os as os
-    import sys as sys
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from astropy.io import fits
+    # Paths, lists & variables
+    path_data = '/n03data/ellien/LSST_ICL/simulations/out4'
+    path_scripts = '/home/ellien/LSST_ICL/scripts'
 
-    # paths
-    path_clusters = '/home/ellien/Euclid_ICL/simulations/out3'
-    path_inpainted = '/home/ellien/Euclid_ICL/simulations/out3'
-    path_star_pos = '/home/ellien/Euclid_ICL/simulations/out3'
+    dirl = ['HorizonAGN', 'Hydrangea', 'Magneticum', 'TNG-100']
 
-    for cluster in glob.glob( os.path.join( path_clusters, 'Challenge?_Euclid?.iptd.fits' ) ):
+    for dir in dirl:
 
-        # filenames
-        fn = cluster.split('/')[-1]
-        cn = fn[:-5]
-        print(cn)
+        image_dir = os.path.join( path_data, dir )
+        image_files = glob.glob(image_dir+'/*.fits')
 
-        rebin_fits( cluster, XBIN = 4, YBIN = 4)
+        for im in image_files:
+
+            rebin_fits( im, XBIN = 4, YBIN = 4, type = 'SUM')
--- synthesis_simulations.py
+++ synthesis_simulations.py
@@ -11,6 +11,9 @@ import dawis as d
 import numpy as np
 import pandas as pd
 from astropy.io import fits
+from astropy.visualization import *
+from scipy.stats import kurtosis
+from cosmo_calc import cosmo_calc
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 def read_image_atoms( nfp, filter_it = None, verbose = False ):
@@ -53,53 +56,180 @@ def read_image_atoms( nfp, filter_it = None, verbose = False ):
 
     return tol, titl
 
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+def synthesis_bcgwavsizesep_with_masks( nfwp, nfap, lvl_sep, lvl_sep_max, lvl_sep_bcg, size_sep, size_sep_pix, xs, ys, n_levels, mscicl, mscbcg, R, N_err, per_err, kurt_filt = True, plot_vignet = False, write_fits = True, measure_PR = False ):
+    '''Wavelet Separation + Spatial filtering.
+    ICL --> Atoms with z > lvl_sep, with maximum coordinates within ellipse mask 'mscell' and with size > size_sep_pix.
+    Galaxies --> Satellites + BCG, so a bit complicated:
+        - Atoms not classified as ICL but with maximum coordinates within ellipse mask 'mscbcg'
+        - Atoms within radius 'rc' of a member galaxy position
+        - In case lvl_sep > 5 (arbitrary...), atoms with z > 5 and within 'mscell' are BCG
+    Unclassified --> rest of atoms
+    '''
+    # path, list & variables
+    icl = np.zeros( (xs, ys) )
+    gal = np.zeros( (xs, ys) )
+    im_art = np.zeros( (xs, ys) )
+    im_unclass = np.zeros( (xs, ys) )
+
+    icl_al = []
+    gal_al = []
+    noticl_al = []
+    unclass_al = []
+
+    # Read atoms
+    ol, itl = read_image_atoms( nfwp, verbose = False )
+
+    # Kurtosis + ICL+BCG
+    for j, o in enumerate(ol):
+
+        x_min, y_min, x_max, y_max = o.bbox
+        sx = x_max - x_min
+        sy = y_max - y_min
+        itm = itl[j].interscale_maximum
+        xco = itm.x_max
+        yco = itm.y_max
+
+        if kurt_filt == True:
+            k = kurtosis(o.image.flatten(), fisher=True)
+            if k < 0:
+                im_art[ x_min : x_max, y_min : y_max ] += o.image
+                continue
+
+        # Remove background
+        if o.level >= lvl_sep_max:
+            continue
+
+        # ICL + BCG
+        if mscicl[xco, yco] != 1:
+
+            # BCG
+            if mscbcg[xco, yco] == 1:
+                icl[ x_min : x_max, y_min : y_max ] += o.image
+                icl_al.append([o, xco, yco])
+
+            # ICL
+            else:
+
+                if (o.level >= lvl_sep) & (sx >= size_sep_pix) & (sy >= size_sep_pix):
+
+                    icl[ x_min : x_max, y_min : y_max ] += o.image
+                    icl_al.append([o, xco, yco])
+                    
+                else:
+                    #gal[ x_min : x_max, y_min : y_max ] += o.image
+                    noticl_al.append([o, xco, yco])
+
+        else:
+            noticl_al.append([ o, xco, yco ])
+
+    if write_fits == True:
+        print('\nWS + SF + SS -- ICL+BCG -- write fits as %s*'%(nfp))
+
+        # write to fits
+        hduo = fits.PrimaryHDU(icl)
+        hduo.writeto( nfap + 'synth.icl.bcgwavsizesepmask_%03d_%03d.fits'%(lvl_sep, size_sep), overwrite = True )
+
+    # Plot vignets
+    if plot_vignet == True:
+
+        interval = AsymmetricPercentileInterval(5, 99.5) # meilleur rendu que MinMax or ZScale pour images reconstruites
+        fig, ax = plt.subplots(2, 2)
+        poim = ax[0][0].imshow(gal, norm = ImageNormalize( gal, interval = interval, stretch = LogStretch()), cmap = 'binary', origin = 'lower')
+        poim = ax[1][0].imshow(icl, norm = ImageNormalize( icl, interval = interval, stretch = LogStretch()), cmap = 'binary', origin = 'lower')
+        poim = ax[0][1].imshow(im_unclass, norm = ImageNormalize( gal, interval = interval, stretch = LogStretch()), cmap = 'binary', origin = 'lower')
+        poim = ax[1][1].imshow(im_art, norm = ImageNormalize( gal, interval = interval, stretch = LogStretch()), cmap = 'binary', origin = 'lower')
+
+        #plt.show()
+        plt.tight_layout()
+        plt.savefig( nfp + 'results.bcgwavsizesepmask_%03d_%03d.png'%(lvl_sep, size_sep), format = 'png' )
+        print('Write vignet to' + nfp + 'synth.bcgwavsizesepmask_%03d_%03d_testspur.png'%(lvl_sep, size_sep))
+        plt.close('all')
+
+    if measure_PR == True:
+
+        # Measure Fractions and uncertainties
+        F_ICL_m, F_ICL_low, F_ICL_up, out_sed =  selection_error(icl_al, unclass_al, M = N_err, percent = per_err, lvl_sep_big = lvl_sep_big, gamma = gamma, xs = xs, ys = ys, flux_lim = flux_lim, mscsedl = mscsedl)
+        F_gal_m, F_gal_low, F_gal_up,_ =  selection_error(gal_al, unclass_al, M = N_err, percent = per_err, lvl_sep_big = lvl_sep_big, gamma = gamma, xs = xs, ys = ys, flux_lim = flux_lim, mscsedl = mscsedl)
+        f_ICL_m = F_ICL_m / (F_ICL_m + F_gal_m)
+        f_ICL_low = F_ICL_low / (F_ICL_low + F_gal_up)
+        f_ICL_up = F_ICL_up / (F_ICL_up + F_gal_low)
+
+        print('\nWS + SF + SS -- ICL+BCG -- z = %d    sise_sep = %d'%(lvl_sep, size_sep))
+        print('N = %4d   F_ICL = %f Mjy/sr  err_low = %f Mjy/sr  err_up = %f Mjy/sr'%(len(icl_al), F_ICL_m, F_ICL_low, F_ICL_up))
+        print('N = %4d   F_gal = %f Mjy/sr  err_low = %f Mjy/sr  err_up = %f Mjy/sr'%(len(gal_al), F_gal_m, F_gal_low, F_gal_up))
+        print('f_ICL = %1.3f    f_ICL_low = %1.3f   f_ICL_up = %1.3f'%(f_ICL_m, f_ICL_low, f_ICL_up))
+
+        # Measure Power ratio
+        results_PR = PR_with_selection_error(atom_in_list = icl_al, atom_out_list = unclass_al, M = N_err, percent = per_err, lvl_sep_big = lvl_sep_big, gamma = gamma, R = R, xs = xs, ys = ys)
+        PR_1_m, PR_1_up, PR_1_low = results_PR[0]
+        PR_2_m, PR_2_up, PR_2_low = results_PR[1]
+        PR_3_m, PR_3_up, PR_3_low = results_PR[2]
+        PR_4_m, PR_4_up, PR_4_low = results_PR[3]
+
+        print('PR_1_m = %1.2e    PR_1_low = %1.2e    PR_1_up = %1.2e'%(PR_1_m, PR_1_low, PR_1_up))
+        print('PR_2_m = %1.2e    PR_2_low = %1.2e    PR_2_up = %1.2e'%(PR_2_m, PR_2_low, PR_2_up))
+        print('PR_3_m = %1.2e    PR_3_low = %1.2e    PR_3_up = %1.2e'%(PR_3_m, PR_3_low, PR_3_up))
+        print('PR_4_m = %1.2e    PR_4_low = %1.2e    PR_4_up = %1.2e'%(PR_4_m, PR_4_low, PR_4_up))
+
+        return icl, gal, F_ICL_m, F_ICL_low, F_ICL_up, F_gal_m, F_gal_low, F_gal_up, f_ICL_m, f_ICL_low, f_ICL_up, PR_1_m, PR_1_up, PR_1_low, PR_2_m, PR_2_up, PR_2_low, PR_3_m, PR_3_up, PR_3_low, PR_4_m, PR_4_up, PR_4_low, out_sed
+
+    else:
+
+        return icl, gal, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, [ np.nan ]
+
+
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if __name__ == '__main__':
 
     # Paths, lists & variables
-    path_data = '/n03data/ellien/Euclid_ICL/simulations/out1/54000066009352'
-    path_wavelets = '/n03data/ellien/Euclid_ICL/wavelets/out1/run3'
-    path_analysis = '/n03data/ellien/Euclid_ICL/analysis/54000066009352/out1/run3'
-    
+    #360009933000018  365000132000018  373000139000019
+    path_data = '/n03data/ellien/Euclid_ICL/simulations/out2/360009933000018'
+    path_wavelets = '/n03data/ellien/Euclid_ICL/wavelets/out2/360009933000018'
+    path_analysis = '/n03data/ellien/Euclid_ICL/analysis/out2/360009933000018' 
     n_lvl = 10
     lvl_sep = 5
+    lvl_sep_max = 1000
+    lvl_sep_bcg = 1000
     dist_sep = 50 # pix
+    size_sep_pix = 100 # pix
+    size_sep = size_sep_pix # NOT kpc for now
     
-    '''for nfp in glob.glob( os.path.join(path_data, '*.fits') ):
+    H0 = 70.0 # Hubble constant
+    Om_M = 0.3 # Matter density
+    Om_Lam = 0.7 # Energy density
+    
+    mscicl = fits.getdata('/n03data/ellien/Euclid_ICL/simulations/out2/mscicl.fits')
+    mscbcg = fits.getdata('/n03data/ellien/Euclid_ICL/simulations/out2/mscbcg.fits')
+    
+    for nfp in glob.glob( os.path.join(path_data, '*.fits') ):
                 
         hdu = fits.open(nfp)
         head = hdu[0].header
         oim = hdu[0].data
         
-        xs, ys = oim.shape
-        xc, yc = xs / 2., ys / 2.
-        
+        xs, ys = oim.shape        
         nf = nfp.split('/')[-1][:-5]
-        nwfp = os.path.join(path_wavelets, nf)
-        ol, itl = read_image_atoms( nfp = nwfp, filter_it = None, verbose = False )
+        nfwp = os.path.join(path_wavelets, nf)
+        nfap = os.path.join(path_analysis, nf)
         
-        # Simple synthesis WS+SS
-        icl = np.zeros(oim.shape)
-
-        for o, it in zip(ol, itl):
-            
-            x_min, y_min, x_max, y_max = o.bbox
-            xo, yo = it.interscale_maximum.x_max, it.interscale_maximum.y_max
-            if (o.level >= lvl_sep) & ( np.sqrt( (xc - xo)**2 + (yc - yo)**2 ) <= dist_sep ):
-                icl[ x_min : x_max, y_min : y_max ] += o.image
-                
-        print(nf, np.sum(icl))        
-        hduo = fits.PrimaryHDU(icl, header = head)
-        hduo.writeto(os.path.join(path_analysis, nf+'.icl.fits'))'''
-
-    path_analysis = '/home/aellien/Euclid_ICL/analysis/out1/run3'
-
+        synthesis_bcgwavsizesep_with_masks( nfwp, nfap, lvl_sep, lvl_sep_max, lvl_sep_bcg,
+                                           size_sep, size_sep_pix, xs, ys,
+                                           n_levels, mscicl, mscbcg,
+                                           N_err = 0,
+                                           per_err = 0,
+                                           kurt_filt = True,
+                                           plot_vignet = True,
+                                           write_fits = True,
+                                           measure_PR = False )
+        
     df = pd.DataFrame([])        
     for z in [ '0.3', '0.6', '0.9', '1.2', '1.5', '1.8' ]:
+        kpc_DA = cosmo_calc(['cosmo_calc.py',str(z),str(H0),str(Om_M),str(Om_Lam)])
         col = []
         for num_vignet in range(1, 9):
-            icl = fits.getdata(os.path.join(path_analysis, 'z_%s_vignet_%d.icl.fits'%(z, num_vignet)))
+            icl = fits.getdata(os.path.join(path_analysis, 'EUC_NIR_W-STK-IMAGE_H_z_%s_fICL0.15_re_1.0_galsim_swarp_grid_bgsub_vignet_%d.synth.icl.bcgwavsizesepmask_%03d_%03d.fits'%(z, num_vignet, lvl_sep, size_sep)))
             Ficl = np.sum(icl)
             col.append(Ficl)
         df[z] = col
